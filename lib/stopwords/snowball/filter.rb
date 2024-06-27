ae762ab5a109944bc0324311275daa1ecf7c14ce
Mariano J. Ayesa <mayesa@gmail.com>
2024-06-27 15:30:48 -0300

    Use File.exist? to be compatible with ruby 3.2

--- lib/stopwords/snowball/filter.rb
+++ lib/stopwords/snowball/filter.rb
@@ -1,16 +1,17 @@
 module Stopwords
   module Snowball
     class Filter < Stopwords::Filter
-      attr_reader :locale
-      attr_reader :locale_filename
+      attr_reader :locale, :locale_filename
 
-      def initialize locale, custom_list = []
+      def initialize(locale, custom_list = [])
         @locale = locale.gsub(/-\w+/, '') # remove country appendix
         @locale_filename = "#{File.dirname(__FILE__)}/locales/#{locale}.csv"
 
-        raise ArgumentError.new("Unknown locale: #{locale.inspect}") unless File.exists?(@locale_filename)
-        super File.read(@locale_filename).split(",") + custom_list
+        raise ArgumentError.new("Unknown locale: #{locale.inspect}") unless File.exist?(@locale_filename)
+
+        super(File.read(@locale_filename).split(',') + custom_list)
       end
     end
   end
 end
+
